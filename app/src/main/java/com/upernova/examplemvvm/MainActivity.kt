84486894e3fcbfee01e6168718de52e72e502c6c
Ana <ac5535680@gmail.com>
2025-10-18 14:08:54 -0600

    Bacgraund

--- app/src/main/java/com/upernova/examplemvvm/MainActivity.kt
+++ app/src/main/java/com/upernova/examplemvvm/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.upernova.examplemvvm.ui.theme.ExampleMVVMTheme
 
@@ -21,7 +23,7 @@ class MainActivity : ComponentActivity() {
             ExampleMVVMTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Ana",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -34,7 +36,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier.background(Color.Magenta)
     )
 }
 
@@ -42,6 +44,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ExampleMVVMTheme {
-        Greeting("Android")
+        Greeting("Ana")
     }
 }
